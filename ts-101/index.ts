9fe54ae5d9119d373648b6eaa25e38cd6760abca
gracenkazi <pleina1@yahoo.fr>
2024-09-05 12:41:29 -0500

    first commit

---
+++ ts-101/index.ts
@@ -0,0 +1,22 @@
+// strings
+const learnerName: string = 'Bill';
+
+const lasName: string = 'Gates';
+
+// number
+let age: number = 28;
+
+// booleans
+let authenticated: boolean = true;
+
+// arrays
+const favNumbers: number[] = [1,2,3];
+const languages: string[] = ['JS', 'TS', 'C#', 'Python']; 
+
+favNumbers.push(11);
+favNumbers.push(33);
+
+languages.push('R');
+languages.push('C++');
+
+const company: {name: string, value: number} = {name: 'Microsoft', value: 30000000}
